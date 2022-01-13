d16dcd5dd6301aaff831849cf75e2113df5a3c5d
Vlad Maraev <lichreg@ya.ru>
2022-01-13 20:55:31 +0100

    operators

--- talk.pl
+++ talk.pl
@@ -4,6 +4,31 @@
 
 ******************************************************/
 
+/*===================================================== 
+    Operators
+=====================================================*/
+
+:- op(500,xfy,&).
+:- op(510,xfy,=>).
+:- op(100,fx,~).   % '
+
+/*===================================================== 
+    Dictionary
+=====================================================*/
+
+/*----------------------------------------------------- 
+    Preterminals
+-----------------------------------------------------*/
+
+%% det(LF) --> [D], {det(D, LF)}. % LF = logical form
+%% n(LF) --> [N], {n(N, LF)}.
+%% pn((E^S)^S) --> [PN], {pn(PN, E)}.
+
+
+%% aux(Form, LF) --> [Aux], {aux(Aux, Form, LF)}.
+%% relpron --> [RP], {relpron(RP)}.
+%% whpron --> [WH], {whpron(WH)}.
+
 
 /*----------------------------------------------------- 
     Lexical Items
@@ -17,16 +42,18 @@ whpron( who ).
 whpron( whom ).
 whpron( what ).
 
-det( every, (XˆS1)ˆ(XˆS2)ˆ   all(X, S1=>S2) ).
-det( a,     (XˆS1)ˆ(XˆS2)ˆexists(X,S1&S2)   ).
-det( some,  (XˆS1)ˆ(XˆS2)ˆexists(X,S1&S2)   ).
+det( every, (X^S1)^(X^S2)^   all(X, S1=>S2) ).
+det( a, (X^S1)^(X^S2)^exists(X, S1&S2)).
 
-n( author	, Xˆ ‘author(X)		).
-n( book		, Xˆ ‘book(X)		).
-n( professor	, Xˆ ‘professor(X)	).
-n( program	, Xˆ ‘program(X)	).
-n( programmer	, Xˆ ‘programmer(X)	).
-n( student	, Xˆ ‘student(X)	).
+det( a,     (X^S1)^(X^S2)^exists(X,S1&S2)   ).
+det( some,  (X^S1)^(X^S2)^exists(X,S1&S2)   ).
+
+n( author	, X^ ~author(X)		).
+n( book		, X^ ~book(X)		).
+n( professor	, X^ ~professor(X)	).
+n( program	, X^ ~program(X)	).
+n( programmer	, X^ ~programmer(X)	).
+n( student	, X^ ~student(X)	).
 pn( begriffsschrift	, begriffsschrift	).
 pn( bertrand		, bertrand		).
 pn( bill		, bill			).
@@ -37,33 +64,33 @@ pn( shrdlu		, shrdlu		).
 pn( terry               , terry                 ).
 
 iv( halt, halts, halted,
-    halted, halting, Xˆ ‘halt(X) ). % VM: halts(X)?
+    halted, halting, X^ ~halt(X) ). % VM: halts(X)?
 
 tv( write, writes, wrote,
-    written, writing, XˆYˆ ‘writes(X,Y) ).
+    written, writing, X^Y^ ~writes(X,Y) ).
 
 tv( meet, meets, met,
-    met, meeting, XˆYˆ ‘meets(X,Y) ).
+    met, meeting, X^Y^ ~meets(X,Y) ).
 
 tv( concern, concerns, concerned,
-    concerned, concerning, XˆYˆ ‘concerns(X,Y) ).
+    concerned, concerning, X^Y^ ~concerns(X,Y) ).
 
 
 tv( run, run, runs, ran, running,
-    XˆYˆ ‘runs(X,Y) ).
+    X^Y^ ~runs(X,Y) ).
 
 rov( want,
      wanted,
      wants, wanted, wanting,
      % semantics is partial execution of
-     % NP ˆ VP ˆ Y ˆ NP( Xˆwant(Y,X,VP(X)) )
-     ((Xˆ ‘want(Y,X,Comp))ˆS) ˆ (XˆComp) ˆ Y ˆ S,
+     % NP ^ VP ^ Y ^ NP( X^want(Y,X,VP(X)) )
+     ((X^ ~want(Y,X,Comp))^S) ^ (X^Comp) ^ Y ^ S,
      % form of VP required:
      infinitival).
 
-aux( to, infinitival/nonfinite, VPˆ VP ).
-aux( does, finite/nonfinite, VPˆ VP ).
-aux( did, finite/nonfinite, VPˆ VP ).
+aux( to, infinitival/nonfinite, VP^ VP ).
+aux( does, finite/nonfinite, VP^ VP ).
+aux( did, finite/nonfinite, VP^ VP ).
 
 /*===================================================== 
         Auxiliary Predicates
@@ -75,6 +102,8 @@ aux( did, finite/nonfinite, VPˆ VP ).
 %%%   List1 ==> a list
 %%%   List2 ==> a list 
 %%%   List <== the concatenation of the two lists
+%%%
+%%% Note: seems to be the same as append/3
 
 conc([], List, List).
 conc([Element|Rest], List, [Element|LongRest]) :-
